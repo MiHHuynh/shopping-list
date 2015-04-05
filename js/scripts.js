d01c7ede538e8ef761874d7d8bcbfd4220a6afac
Michelle Huynh <michehuynh@yahoo.com>
2015-04-04 18:27:02 -0700

    Tried to get the add-item function going, but having issues with collapsing divs/spans and not grabbing value from field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,41 @@
-$(document).ready(){
+$(document).ready(function(){
+	var counter = 0;
+	var itemRow =
+		'<div class="item-row">' +
+		 	'<div class="remove-x">' +
+		 		'<i class="fa fa-times fa-2x"></i>' + 
+		 	'</div>' +
+
+		 	//the height of div #item shows up 10px. why?
+		 	'<div class="item">' +
+
+		 		//why does span show up without content in it 
+		 		//and with a height of only 5px?
+		 		'<span class="item">' + $('#field').val() + '</span>' + 
+
+
+		 	'</div>' + 
+		 	'<div class="check">' +
+		 		'<i class="fa fa-check fa-2x"></i>' +
+		 	'</div>' +
+		'</div>';
+
+// WHAT TO DO NEXT: 
+// ASSIGN ID TO EACH THING, LEAVE CLASS UNCHANGED SO THAT CSS DOESN'T BREAK
+
 	$(document).keydown(function(e){
 		if (e.which == 13) {
-			$('.shell').prepend('Hi');
-		};
+			//var content = $('#field').val();
+			$('.items').prepend(itemRow);
+			$('#field').val('');
+
+		}; //end if statement
 
 
 
-	});
-	// var counter = 0;
-	// $(document).keydown(function(e){
-	// 	var field = $('#field').val();
-	// 	var line = 
-	// 		'<h1>hello</h1>';
+	}); //end keydown
 
-	// 	//enter key pressed
-	// 	while (e.which == 13){
-	// 		$(".shell").prepend(line);
-	// 		$("#field").val('');
-	// 		counter++;
-	// 		alert (counter);
-	// 	};
 		//$(".x").click(function (){
 		//	$(this).remove();
 		//});
-	});
-
-
-
-
-
-
-}; //end document ready
+}); //end document ready
